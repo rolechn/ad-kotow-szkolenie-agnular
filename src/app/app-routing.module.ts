68adbbd6aafb3e3aadea71a896003cc77ffd00a6
Natalia Zawadzka <nzawadzka@deloittece.com>
2022-07-20 16:02:14 +0200

    add icons about vacinated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CatsDataResolver } from './cats/cats-overview/cats-data-resolver';
 import { CatsOverviewComponent } from './cats/cats-overview/cats-overview.component';
 import { CatsDetailViewComponent } from './cats/cats-detail-view/cats-detail-view.component';
 
+
 export const appRoutes: Routes = [
   {
     path: '',
